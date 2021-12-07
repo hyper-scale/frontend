97402cf6629f5544e26bd07d11ef4137b403e62f
KierenB <4819592+KierenB@users.noreply.github.com>
2021-12-06 20:16:56 -0800

    Create privacy policy and add google analytics
    - Created privacy policy text, it needs to be linked from the footer (suggest "Privacy" link)
    - Added google analytics script to the header

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,6 +59,17 @@ const timeline = [
 const Landing = () => (
 	<div>
 		<Meta />
+		<head>
+			<!-- Global site tag (gtag.js) - Google Analytics -->
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-6Q4FQEBFKC"></script>
+			<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'G-6Q4FQEBFKC');
+			</script>
+		</head>
 		<div
 			className="relative bg-white overflow-hidden"
 			style={{
